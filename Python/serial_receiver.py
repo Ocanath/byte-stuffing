b89e881107fe008860363f5278c7f23cdb9b6f8d
ocanath <ocanath@gmail.com>
2023-09-12 18:47:35 -0700

    created template serial connecting for transmitter-recevier demo

---
+++ Python/serial_receiver.py
@@ -0,0 +1,46 @@
+import serial
+from serial.tools import list_ports
+from PPP_stuffing import *
+import argparse
+
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description='Serial PPP stuffing application parser')
+	parser.add_argument('--CP210x_only', help="If you're finding a non serial adapter, use this to filter", action='store_true')
+	args = parser.parse_args()
+
+	slist = []	
+	""" 
+		Find all serial ports.
+	"""
+	com_ports_list = list(list_ports.comports())
+	port = []
+
+	for p in com_ports_list:
+		if(p):
+			pstr = ""
+			pstr = p
+			port.append(pstr)
+			print("Found:", pstr)
+	if not port:
+		print("No port found")
+
+	for p in port:
+		try:
+			ser = []
+			if( (args.CP210x_only == False) or  (args.CP210x_only == True and (p[1].find('CP210x') != -1) or p[1].find('USB Serial Port') != -1) ):
+				ser = (serial.Serial(p[0],'460800', timeout = 1))
+				slist.append(ser)
+				print ("connected!", p)
+				break
+		except:
+			print("failded.")
+			pass
+
+
+
+
+
+	for s in slist:
+		s.close()
