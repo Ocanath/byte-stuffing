2ebda1af5aa740b9b0f7165c05d40ebb9cecd367
ocanath <ocanath@gmail.com>
2026-04-21 13:52:53 -0700

    created a generalized solution to input stream buffering with delimiter characters

---
+++ bytestream/bytestream.c
@@ -0,0 +1,53 @@
+#include "bytestream.h"
+
+/*
+Basic check function
+*/
+int check_buf(bytestream_buf_t * buf)
+{
+	if(buf == NULL)
+	{
+		return STREAM_ERROR_NULLPTR;
+	}
+	else if (buf->buf == NULL)
+	{
+		return STREAM_ERROR_NULLPTR;
+	}
+	else if(buf->size = 0)
+	{
+		return STREAM_ERROR_EMPTYBUFFER;
+	}
+	else if(buf->len > buf->size)
+	{
+		return STREAM_ERROR_OVERRUN;
+	}
+	return STREAM_SUCCESS;
+}
+
+/*
+Unified logic for delimiter character based stream decoding
+*/
+int bytestream(unsigned char new_byte, bytestream_buf_t * input, unsigned char delimiter)
+{
+	int rc = check_buf(input);
+	if(rc != STREAM_SUCCESS)
+	{
+		return rc;
+	}
+
+	if(input->pos >= input->size)
+	{
+		input->pos = 0;
+		return STREAM_ERROR_OVERRUN;
+	}
+
+	input->buf[input->pos++] = new_byte;
+
+	if(new_byte == delimiter)
+	{
+		input->len = input->pos;
+		input->pos = 0;
+		return STREAM_SUCCESS;	//mark "input" as valid for decode
+	}
+	return STREAM_IN_PROGRESS;
+}
---
+++ bytestream/bytestream.h
@@ -0,0 +1,25 @@
+#ifndef BYTESTREAM_H
+#define BYTESTREAM_H
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct bytestream_buf_t
+{
+    unsigned char * buf;	//pointer to the first address of the byte array backing store
+    size_t size;			//size of the backing store buf points to.
+    size_t len;				//length of the message within buf. Reported value. Only valid after returning STREAM_SUCCESS - stale otherwise
+	size_t pos;				//position, for stream buffering purposes. 
+}bytestream_buf_t;
+
+enum {
+	STREAM_SUCCESS = 0,
+	STREAM_IN_PROGRESS = 1,
+	STREAM_ERROR_NULLPTR = -1,
+	STREAM_ERROR_OVERRUN = -2,
+	STREAM_ERROR_EMPTYBUFFER = -3
+};
+
+int check_buf(bytestream_buf_t * buf);
+
+
+#endif
